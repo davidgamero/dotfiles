b2aad67c2858628de036f9003c1099d0b2969f41
David Gamero <david340804@gmail.com>
2024-11-15 23:10:39 -0500

    Create setup-mac.sh

---
+++ setup-mac.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# install homebrew
+if ! command -v brew 2>&1 >/dev/null
+then
+	echo "homebrew not found, installing homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "homebrew found..."
+fi
+
+# install zsh
+if [[ "$(basename "$SHELL")" != "zsh" ]]; then
+	echo "Current shell is not zsh, installing..."
+	brew install zsh
+	chsh -s $(which zsh)
+else
+	echo "current shell is already zsh"	
+fi
+
+# install ohmyzsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	echo "Oh My Zsh not found, installing..."
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+	echo "ohmyzsh already found..."
+fi
+
+# install neovim
+if ! command -v nvim >/dev/null 2>&1; then
+    echo "neovim install not found, installing..."
+    brew install neovim
+else
+    echo "neovim install found..."
+fi
