9ea440669e27c0be117fa7f40497c467d2177138
David Gamero <david340804@gmail.com>
2025-02-13 11:54:14 -0500

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -82,3 +82,8 @@ sudo apt install code # or code-insiders
 
 # golang
 wget https://git.io/go-installer.sh && bash go-installer.sh
+
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
