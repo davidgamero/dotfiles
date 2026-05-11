ba0f2e8928dd338b23b1b6d0db91e3bed5108abd
David Gamero <david340804@gmail.com>
2026-05-11 13:16:23 -0400

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -15,7 +15,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-git clone https://github.com/NvChad/starter ~/.config/nvim && nvim # nvchad
+git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 # base updates
 sudo apt update -y
